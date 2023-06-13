21b47d5d15993c6c964cb2fc1338f81b02521d58
mrtrvl <mrt@mrt.ee>
2023-06-13 17:44:42 +0300

    Ebavajalike kommentaaride eemaldamine

--- development/api/src/components/auth/service.ts
+++ development/api/src/components/auth/service.ts
@@ -18,8 +18,6 @@ const loginService = {
     const user: any = await userService.getUserByEmail(email);
     if (user === undefined) return undefined;
     if (!user) return false;
-    // const match = await hashService.match(password, user.password);
-    // if (!match) return "0";
     const token = await jwtService.sign(user);
     return { token, user };
   },
